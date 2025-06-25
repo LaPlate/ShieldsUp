e4369887e312083239557184cfbdbfd22bd10bc1
LaPlate <ericlaplatney@hotmail.com>
2025-06-25 16:35:38 -0700

    Extension Hell

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,6 +1,14 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Main : Node2D
 {
+    public void main()
+    {
+        while (true)
+        {
+            GD.Print("What is happening");
+        }
+    }
 }
